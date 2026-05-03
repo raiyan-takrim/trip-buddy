31803262a1e5cca4f069d34423e014993e43e056
Md. Raiyan Takrim <raiyan.takrim3@gmail.com>
2026-05-03 12:03:50 +0800

    feat: Adds scheduling to automated cleaning up expired bookings

--- src/main/java/me/raiyantakrim/tripbuddy/repository/BookingRepository.java
+++ src/main/java/me/raiyantakrim/tripbuddy/repository/BookingRepository.java
@@ -2,8 +2,14 @@ package me.raiyantakrim.tripbuddy.repository;
 
 import me.raiyantakrim.tripbuddy.entity.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public interface BookingRepository extends JpaRepository<Booking, UUID> {
+    @Query("SELECT b FROM Booking b WHERE b.status = PENDING AND b.createdAt <= :expirationTime")
+    List<Booking> findExpiredBookings(@Param("expirationTime") LocalDateTime expirationTime);
 }
---
+++ src/main/java/me/raiyantakrim/tripbuddy/service/BookingCleanupService.java
@@ -0,0 +1,53 @@
+package me.raiyantakrim.tripbuddy.service;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import me.raiyantakrim.tripbuddy.entity.Booking;
+import me.raiyantakrim.tripbuddy.entity.Seat;
+import me.raiyantakrim.tripbuddy.repository.BookingRepository;
+import me.raiyantakrim.tripbuddy.repository.SeatRepository;
+import me.raiyantakrim.tripbuddy.utility.BookingStatus;
+import me.raiyantakrim.tripbuddy.utility.SeatStatus;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BookingCleanupService{
+    private final BookingRepository bookingRepository;
+    private final SeatRepository seatRepository;
+
+    @Scheduled(fixedRate = 60000)
+    @Transactional
+    public void releaseExpiredHolds(){
+        // Calculate the 10 min ago time
+        LocalDateTime tenMinuteAge = LocalDateTime.now().minusMinutes(10);
+
+        // Fetch all expired Bookings
+        List<Booking> expiredBookings = bookingRepository.findExpiredBookings(tenMinuteAge);
+        if(expiredBookings.isEmpty()){
+            return;
+        }
+
+        System.out.println("Found " +  expiredBookings.size() + " expired bookings. Cleaning up...");
+
+        // Loop through and release the seats
+        for(Booking booking : expiredBookings){
+            // Mark the Booking as CANCELLED
+            booking.setStatus(BookingStatus.CANCELLED);
+
+            // Get the associate Seat and make it back to AVAILABLE
+            Seat seat = booking.getSeat();
+            seat.setStatus(SeatStatus.AVAILABLE);
+
+            // Save the updates
+            seatRepository.save(seat);
+            bookingRepository.save(booking);
+
+            System.out.println("Released seat " + seat.getSeatNumber() + " for booking ID: " + booking.getId());
+        }
+    }
+}
